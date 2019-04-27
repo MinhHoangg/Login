b52de6b0ae8eb8e3b54f31ad80f0044b2d09618c
Minh Hoangg <minhgato@gmail.com>
2019-04-28 00:06:16 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -44,6 +44,6 @@ list-style: none;
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
-$query = "INSERT INTO toy VALUES ('$_POST[iname]','$_POST[price]')";
+$query = "INSERT INTO toy VALUES ('$_POST[iname]','$_POST[price]','$_POST[quantity]')";
 $result = pg_query($query); 
 ?>
