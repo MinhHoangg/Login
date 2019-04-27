7220927bbe3340756002a95b1ab4b5ef04e216fd
Minh Hoangg <minhgato@gmail.com>
2019-04-27 22:15:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,7 @@
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 $result = pg_query($db,"SELECT * FROM toy");
-echo "<table>";
-?>
-<div class="body">
-	<?php
+echo "<table class = "body">";
 while($row=pg_fetch_assoc($result)){echo "<tr>";
 echo "<td align='center' width='200'>" . $row['iname'] . "</td>";
 echo "<td align='center' width='200'>" . $row['price'] . "</td>";
