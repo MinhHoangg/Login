965917ac4135446603b04a03900f846ec5837dc3
Minh Hoangg <minhgato@gmail.com>
2019-04-27 23:35:07 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -28,7 +28,7 @@
 </style>
 </head>
 <body align = "center">
-<h1>List of all toy in the ATN Shop</h1>
+<h1>Admin Page of ATN Shop</h1>
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
