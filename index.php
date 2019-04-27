8ea9207ac3f3169a16a82e8537d9151753f934ee
Minh Hoangg <minhgato@gmail.com>
2019-04-27 22:13:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <meta charset="utf-8">
 <title>All Product</title>
 <meta name="description" content="If we want to fetch all rows from the actor table the following PostgreSQL SELECT statement can be used.">
+<style type="text/css">
+	.body{
+	border-width:5px;  
+    border-style:inset;
+	}
+</style>
 </head>
 <body>
 <h1>List of all toy in the ATN Shop</h1>
@@ -12,6 +18,9 @@ $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 $result = pg_query($db,"SELECT * FROM toy");
 echo "<table>";
+?>
+<div class="body">
+	<?php
 while($row=pg_fetch_assoc($result)){echo "<tr>";
 echo "<td align='center' width='200'>" . $row['iname'] . "</td>";
 echo "<td align='center' width='200'>" . $row['price'] . "</td>";
