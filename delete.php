a820d6285737ccb03ca981985a9102f68b7b975f
Minh Hoangg <minhgato@gmail.com>
2019-04-27 22:36:38 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+<title>Insert</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+li {
+list-style: none;
+}
+</style>
+</head>
+<body>
+<h2>Delete data from toy table</h2>
+<ul>
+<form name="delete" action="delete.php" method="POST" >
+<li>Toy Name: </li><li><input type="text" name="iname" /></li>
+<li><input type="submit" /></li>
+</form>
+</ul>
+</body>
+</html>
+<?php
+$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
+ port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
+$query = "DELETE FROM toy WHERE iname = ('$_POST[iname]')";
+$result = pg_query($query); 
+?>
