3ffe7822f1ee87e21091166b76dfda4fb281270a
Minh Hoangg <minhgato@gmail.com>
2019-04-27 20:48:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta name="description" content="If we want to fetch all rows from the actor table the following PostgreSQL SELECT statement can be used.">
 </head>
 <body>
-<h1>List of all actors in the table</h1>
+<h1>List of all toy in the ATN Shop</h1>
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
