6bde6541d709b65fae95a364804b7b95e2b8dd19
Minh Hoangg <minhgato@gmail.com>
2019-04-27 23:40:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
 <meta charset="utf-8">
 <title>All Product</title>
 <meta name="description" content="If we want to fetch all rows from the actor table the following PostgreSQL SELECT statement can be used.">
-
+<style type="text/css">
+	body  {
+  background-image: url("paper.gif");
+  
+}
+</style>
 </head>
-<body align = "center">
+<body align = "center" >
 <h1>List of all toy in the ATN Shop</h1>
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
