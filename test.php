946a7d1407367b47b0a0b53e21824da767b929d8
Minh Hoangg <minhgato@gmail.com>
2019-04-29 13:20:34 +0700

    Update test.php

--- test.php
+++ test.php
@@ -41,7 +41,7 @@ table, td, th {
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
-$result = pg_query($db,"SELECT * FROM toy");
+$result = pg_query($db,"SELECT * FROM label");
 ?>
 <table align="center" id="customers">
 	<tr><td align='center' width='200'>Name</td><td align='center' width='200'>Price ($)</td><td align='center' width='200'>Quantity</td></tr>
