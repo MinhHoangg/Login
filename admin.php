9661d7fe655c41a741af3086db15015c14652922
Minh Hoangg <minhgato@gmail.com>
2019-04-29 13:20:02 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -58,7 +58,7 @@ table, td, th {
   color: white;
 </style>
 </head>
-<body align = "center" background="pic.png">
+<body align = "center" background="img/pic.png">
 <h1>Admin Page of ATN Shop</h1>
 <?php
 $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
