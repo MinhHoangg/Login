70daa5feb0d2a283206d44e965b3fc5ecae051ec
Minh Hoangg <minhgato@gmail.com>
2019-04-30 02:09:39 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>All Product</title>
+<meta name="description" content="Assignment 2">
+<style type="text/css">
+table {
+  border-collapse: collapse;
+}
+
+table, td, th {
+  border: 1px solid black;
+}
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #FFFFFF;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: center;
+  background-color: #DC143C;
+  color: white;
+}
+</style>
+</head>
+<body align = "center" background="img/pic.png">
+<h1>List of all toy in the ATN Shop</h1>
+<?php
+$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
+ port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
+$result = pg_query($db,"SELECT * FROM label");
+?>
+<table align="center" id="customers">
+	<tr><th align='center' width='200'>Name</th><th align='center' width='200'>Price ($)</th><th align='center' width='200'>Quantity</th></tr>
+</table>
+<?php
+echo "<table align='center' id='customers'>";
+while($row=pg_fetch_assoc($result)){echo "<tr>";
+echo "<td align='center' width='200'>" . $row['id'] . "</td>";
+echo "<td align='center' width='200'>" . $row['name'] . " $" . "</td>";
+echo "<td align='center' width='200'>" . ?><img src="<?=$row['image']?>" alt=""><?php . "</td>";
+echo "</tr>";}echo "</table>";?>
+</div>
+</body>
+</html>
