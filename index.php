6669e03b170130b3b438d301a9ca0fcc40663c95
Minh Hoangg <minhgato@gmail.com>
2019-05-01 01:01:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 $result = pg_query($db,"SELECT * FROM toy");
 ?>
-<div class="content" id="start-btn">
+<div class="content">
 <table align="center" id="customers">
 	<tr><th align='center' width='200'>Name</th><th align='center' width='200'>Price ($)</th><th align='center' width='200'>Quantity</th><th align='center' width='200'>Image</th></tr>
 </table>
