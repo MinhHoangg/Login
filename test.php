2b4cada16fb41d3b9907bf151e18d8293a1399e6
Minh Hoangg <minhgato@gmail.com>
2019-04-30 21:41:06 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,117 +1,118 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>All Product</title>
-<meta name="description" content="Assignment 2">
-<style type="text/css">
-<head>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-    <title>Imgur: The magic of the Internet</title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8">
-    <meta name="viewport" content="width=1138">
-    <meta name="robots" content="follow, index">
-    <meta name="keywords" content="images, photos, gif, gifs, memes, pictures, new pictures, reaction gifs, share photos, share images, latest images, funny, cute, visual storytelling, imgur">
-    <meta name="description" content="Imgur is the easiest way to discover and enjoy the magic of the Internet.">
-    <meta name="copyright" content="Copyright 2019 Imgur, Inc.">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge;">
-    
-    <link rel="icon" type="image/png" href="//s.imgur.com/images/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="//s.imgur.com/images/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="//s.imgur.com/images/favicon-16x16.png" sizes="16x16">
-    <link rel="apple-touch-icon-precomposed" href="//s.imgur.com/images/favicon-152.png">
-    <meta name="msapplication-TileColor" content="#2cd63c">
-    <meta name="msapplication-TileImage" content="//s.imgur.com/images/favicon-144.png">
-            <link rel="stylesheet" type="text/css" href="//s.imgur.com/min/global.css?1556561330">
+<html>
+    <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="html2canvas.min.js"></script>
+    <script src="chance.min.js"></script>
+    <script src="jquery-2.1.4.js"></script>
+    <script src="jquery-ui-1.9.2.custom.min.js"></script>
+    </head>
+    <body>
 
-    
-    
-            <!--[if IE 9]><link rel="stylesheet" href="//imgur.com/include/css/ie-sucks.css?0" type="text/css" /><![endif]-->
-    
-            <link rel="canonical" href="https://imgur.com/upload">
-        <meta property="og:url" content="https://imgur.com/upload">
-    
-            <link rel="alternate" media="only screen and (max-width: 640px)" href="https://m.imgur.com/upload">
-    
-    <meta name="p:domain_verify" content="834554521765408b9effdc758b69c5ee">
-    <meta property="og:site_name" content="Imgur">
-    <meta property="fb:admins" content="12331492">
-    <meta property="fb:admins" content="12301369">
-    <meta property="fb:app_id" content="127621437303857">
-    <meta property="al:android:url" content="imgur://imgur.com/upload?from=fbreferral">
-    <meta property="al:android:app_name" content="Imgur">
-    <meta property="al:android:package" content="com.imgur.mobile">
-    <meta property="al:ios:url" content="imgur://imgur.com/upload?from=fbreferral">
-    <meta property="al:ios:app_store_id" content="639881495">
-    <meta property="al:ios:app_name" content="Imgur">
-    <meta property="al:web:url" content="https://imgur.com/upload">
-    <meta name="twitter:site" content="@imgur">
-    <meta name="twitter:domain" content="imgur.com">
-    <meta name="twitter:app:id:googleplay" content="com.imgur.mobile">
-            <meta property="og:title" content="Imgur">
-        <meta property="og:type" content="website">
-        <meta property="og:description" content="Imgur: The magic of the Internet">
-        <meta property="og:image" content="https://s.imgur.com/images/logo-1200-630.jpg?2">
-        <meta property="og:image:height" content="630">
-        <meta property="og:image:width" content="1200">
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="Imgur">
-        <meta name="twitter:description" content="Imgur: The magic of the Internet">
-        <meta name="twitter:image:src" content="//s.imgur.com/images/logo-1200-630.jpg?2">
-        <meta name="twitter:image:height" content="630">
-        <meta name="twitter:image:width" content="1200">
-        <meta name="twitter:creator" content="@imgur">
-    
+      <div class="content">
+        <img src="burger.png" height="600">
+        <button id="start-btn">Snap!</button>
+      </div>   
 
+    <script> 
     
-    <script type="text/javascript" async="" src="https://sb.scorecardresearch.com/c2/7770950/cs.js"></script><script src="https://quantcast.mgr.consensu.org/cmp.js" async="" type="text/javascript"></script><script id="twitter-wjs" src="https://platform.twitter.com/widgets.js"></script><script async="" src="//c.amazon-adsystem.com/aax2/apstag.js"></script><script src="https://rules.quantcount.com/rules-p-f8oruOqDFlMeI.js" async=""></script><script src="https://connect.facebook.net/signals/config/742377892535530?v=2.8.47&amp;r=stable" async=""></script><script async="" src="//connect.facebook.net/en_US/fbevents.js"></script><script type="text/javascript" async="" src="https://secure.quantserve.com/quant.js"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-M6N38SF"></script><script type="application/ld+json">
-        {
-            "@context": "http://schema.org",
-            "@type": "WebSite",
-            "url": "https://imgur.com",
-            "name": "Imgur",
-            "potentialAction": {
-                "@type": "SearchAction",
-                "target": "https://imgur.com/search?q={search_term_string}",
-                "query-input": "required name=search_term_string"
-            }
+    var imageDataArray = [];
+    var canvasCount = 35;
+    $("#start-btn").click(function(){
+      
+      html2canvas($(".content")[0]).then(canvas => {
+        //capture all div data as image
+        ctx = canvas.getContext("2d");
+        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var pixelArr = imageData.data;
+        createBlankImageData(imageData);
+        //put pixel info to imageDataArray (Weighted Distributed)
+        for (let i = 0; i < pixelArr.length; i+=4) {
+          //find the highest probability canvas the pixel should be in
+          let p = Math.floor((i/pixelArr.length) *canvasCount);
+          let a = imageDataArray[weightedRandomDistrib(p)];
+          a[i] = pixelArr[i];
+          a[i+1] = pixelArr[i+1];
+          a[i+2] = pixelArr[i+2];
+          a[i+3] = pixelArr[i+3]; 
         }
+        //create canvas for each imageData and append to target element
+        for (let i = 0; i < canvasCount; i++) {
+          let c = newCanvasFromImageData(imageDataArray[i], canvas.width, canvas.height);
+          c.classList.add("dust");
+          $("body").append(c);
+        }
+        //clear all children except the canvas
+        $(".content").children().not(".dust").fadeOut(3500);
+        //apply animation
+        $(".dust").each( function(index){
+          animateBlur($(this),0.8,800);
+          setTimeout(() => {
+            animateTransform($(this),100,-100,chance.integer({ min: -15, max: 15 }),800+(110*index));
+          }, 70*index); 
+          //remove the canvas from DOM tree when faded
+          $(this).delay(70*index).fadeOut((110*index)+800,"easeInQuint",()=> {$( this ).remove();});
+        });
+      });
+    });
+    function weightedRandomDistrib(peak) {
+      var prob = [], seq = [];
+      for(let i=0;i<canvasCount;i++) {
+        prob.push(Math.pow(canvasCount-Math.abs(peak-i),3));
+        seq.push(i);
+      }
+      return chance.weighted(seq, prob);
+    }
+    function animateBlur(elem,radius,duration) {
+      var r =0;
+      $({rad:0}).animate({rad:radius}, {
+          duration: duration,
+          easing: "easeOutQuad",
+          step: function(now) {
+            elem.css({
+                  filter: 'blur(' + now + 'px)'
+              });
+          }
+      });
+    }
+    function animateTransform(elem,sx,sy,angle,duration) {
+      var td = tx = ty =0;
+      $({x: 0, y:0, deg:0}).animate({x: sx, y:sy, deg:angle}, {
+          duration: duration,
+          easing: "easeInQuad",
+          step: function(now, fx) {
+            if (fx.prop == "x") 
+              tx = now;
+            else if (fx.prop == "y") 
+              ty = now;
+            else if (fx.prop == "deg") 
+              td = now;
+            elem.css({
+                  transform: 'rotate(' + td + 'deg)' + 'translate(' + tx + 'px,'+ ty +'px)'
+              });
+          }
+      });
+    }
+    function createBlankImageData(imageData) {
+      for(let i=0;i<canvasCount;i++)
+      {
+        let arr = new Uint8ClampedArray(imageData.data);
+        for (let j = 0; j < arr.length; j++) {
+            arr[j] = 0;
+        }
+        imageDataArray.push(arr);
+      }
+    }
+    function newCanvasFromImageData(imageDataArray ,w , h) {
+      var canvas = document.createElement('canvas');
+          canvas.width = w;
+          canvas.height = h;
+          tempCtx = canvas.getContext("2d");
+          tempCtx.putImageData(new ImageData(imageDataArray, w , h), 0, 0);
+          
+      return canvas;
+    }
     </script>
-    
-    <!-- Google Tag Manager -->
-<script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-M6N38SF');</script>
-<!-- End Google Tag Manager -->
-
-    <!--[if lte IE 8]><script type="text/javascript" src="//s.imgur.com/min/iepoly.js?1556561330"></script>
-<![endif]-->
-    
-<link type="text/css" rel="stylesheet" href="//s.imgur.com/min/gallery.css?1556561330"><script charset="utf-8" src="https://platform.twitter.com/js/button.dd024c345fc26f7c7a8d9938b67e5d3d.js"></script></head>
-</style>
-</head>
-<body align = "center" background="img/pic.png">
-<!-- <h1>List of all toy in the ATN Shop</h1>
-<?php
-$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
- port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
-$result = pg_query($db,"SELECT * FROM label");
-?>
-<table align="center" id="customers">
-	<tr><th align='center' width='200'>Name</th><th align='center' width='200'>Price ($)</th><th align='center' width='200'>Quantity</th></tr>
-</table> -->
-<input type="file" id="global-files-button" class="nodisplay" name="files[]" multiple="" accept=".jpg,.jpeg,.png,.gif,.apng,.tiff,.tif,.bmp,.xcf,.webp,.mp4,.mov">
-<label class="browse-btn" for="global-files-button">Browse</label>
-<!-- <?php
-echo "<table align='center' id='customers'>";
-while($row=pg_fetch_assoc($result)){echo "<tr>";
-echo "<td align='center' width='200'>" . $row['id'] . "</td>";
-echo "<td align='center' width='200'>" . $row['name'] . " $" . "</td>";
-echo "<td align='center' width='200'>";?><img src="/<?php echo $row['image']; ?>" height="100" width="100"> <?php "</td>";
-echo "</tr>";}echo "</table>";?> -->
-</div>
-</body>
+    </body>
 </html>
