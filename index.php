5459d0009edcf87c647cbda513a919d38112499d
Minh Hoangg <minhgato@gmail.com>
2019-04-30 23:30:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,11 +63,6 @@ table, td, th {
   top: 0px;
   right: 0px;
 }
-.fixed2{
-  position: fixed;
-  top: 0px;
-  left: 0px;
-}
 * {
   box-sizing: border-box;
 }
@@ -106,8 +101,6 @@ $db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 $result = pg_query($db,"SELECT * FROM toy");
 ?>
-<div class="content">
-<div class="fixed2"><button id="start-btn" class="button button5">Snap!</button></div>
 <table align="center" id="customers">
 	<tr><th align='center' width='200'>Name</th><th align='center' width='200'>Price ($)</th><th align='center' width='200'>Quantity</th><th align='center' width='200'>Image</th></tr>
 </table>
