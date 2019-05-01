e54c33e1aadd1ad4a3f61650ec699dd8bfeb6916
Minh Hoangg <minhgato@gmail.com>
2019-05-01 23:08:51 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+	$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
+ port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
+	
+	$myusername = $_POST['user'];
+	$mypassword = $_POST['pass'];
+	
+	$myusername = stripslashes($myusername);
+	$mypassword = stripslashes($mypassword);
+	
+	$query = "SELECT * FROM login WHERE users='$myusername' and pass='$mypassword'";
+	$result = mysql_query($query);
+	$count = mysql_num_rows($result);
+	
+	mysql_close();
+	
+	if($count==1){
+		$seconds = 5 + time();
+		setcookie(loggedin, date("F jS - g:i a"), $seconds);
+		header("location:admin.php");
+	}else{
+		echo 'Incorrect Username or Password';
+	}
+?>
