74f5cb1e04571056379fa3c7a35ab53c980b44a0
Minh Hoangg <minhgato@gmail.com>
2019-05-02 00:02:47 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,8 +7,6 @@
 	$count = pg_num_rows($result);
 	
 	if($count==1){
-		$seconds = 5 + time();
-		setcookie(loggedin, date("F jS - g:i a"), $seconds);
 		header("location:admin.php");
 	}else{
 		echo 'Incorrect Username or Password';
