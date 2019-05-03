59f2d50439de694af5a5f540d352584f16e837e7
Minh Hoangg <minhgato@gmail.com>
2019-05-03 23:11:58 +0700

    Update permission.php

--- permission.php
+++ permission.php
@@ -2,8 +2,5 @@
 if (isset($_SESSION['users']) == false) {
 	// Nếu người dùng chưa đăng nhập thì chuyển hướng website sang trang đăng nhập
 	header("location: index.php");
-}else {
-	echo "Bạn không đủ quyền truy cập vào trang này<br>";
-			echo "<a href='https://parkboyoung.herokuapp.com/index.php'> Click để về lại trang chủ</a>";
 }
 ?>
