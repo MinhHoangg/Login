1e24b7f5112d14b2031368e3b1cd5f8e0c83bc0a
Minh Hoangg <minhgato@gmail.com>
2019-05-04 08:05:11 +0700

    Update permission.php

--- permission.php
+++ permission.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_SESSION["user"]) == false) {
+if (isset($_SESSION["users"]) == false) {
 	// Nếu người dùng chưa đăng nhập thì chuyển hướng website sang trang đăng nhập
 	header("location: index.php");
 } else{header("location: admin.php");}
