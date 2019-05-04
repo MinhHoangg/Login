590c6cb3684458e7e01c7146ce93edd35f9a691e
Minh Hoangg <minhgato@gmail.com>
2019-05-04 08:17:28 +0700

    m

--- permission.php
+++ permission.php
@@ -1,6 +1,5 @@
 <?php
 if (isset($_SESSION["users"]) == false) {
-	// Nếu người dùng chưa đăng nhập thì chuyển hướng website sang trang đăng nhập
 	header("location: index.php");
 } else{header("location: admin.php");}
 ?>
