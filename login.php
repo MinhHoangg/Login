9bdd6564f279c41c468976991c7ec338e7197312
Minh Hoangg <minhgato@gmail.com>
2019-05-04 10:16:59 +0700

    h

--- login.php
+++ login.php
@@ -5,13 +5,12 @@ session_start();
 	$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 	
-	$query = "SELECT * FROM login WHERE users='$_POST['user']' and pass='$_POST['pass']'";
+	$query = "SELECT * FROM login WHERE users='$_POST[user]' and pass='$_POST[pass]'";
 	$result = pg_query($db,$query);
 	$count = pg_num_rows($result);
 	
 	if($count==1){
-	$data = pg_fetch_array($query);
-	$_SESSION['user'] = $data['users'];
+	$_SESSION['user'] = true;
 	    
 	echo "<meta http-equiv='refresh' content='0;url=admin.php' />";
 	}else{
--- permission.php
+++ permission.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['user'])) {
+if ($_SESSION['user'] == true) {
 	header("location: admin.php");
 } else{header("location: index.php");}
 ?>
