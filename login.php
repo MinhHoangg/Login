def1a1165a6e023162361ab350bcd8783625cb4f
Minh Hoangg <minhgato@gmail.com>
2019-05-04 09:49:41 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 	
 	$query = "SELECT * FROM login WHERE users='$_POST[user]' and pass='$_POST[pass]'";
-	$result = pg_query($query);
+	$result = pg_query($db,$query);
 	$count = pg_num_rows($result);
 	
 	if($count==1){
