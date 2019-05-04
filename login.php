4b13ff1c715b6a5c88423fa2b517b8dc78a5047b
Minh Hoangg <minhgato@gmail.com>
2019-05-04 08:51:56 +0700

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,6 @@ session_start();
 	$db = pg_connect("host=ec2-54-235-114-242.compute-1.amazonaws.com
  port=5432 dbname=d8sto9amrrhb0v user=bsodvujawdtmnt password=e5eb56a54ac2393fca0715e5f0d9e4e8c5c1b5cee85b45edf957bb2c30fbcc4b");
 	
-	if(isset($_POST['submit'])){
 	$query = "SELECT * FROM login WHERE users='$_POST[user]' and pass='$_POST[pass]'";
 	$result = pg_query($query);
 	$count = pg_num_rows($result);
@@ -19,5 +18,4 @@ session_start();
 	}else{
 		header("location:index.php");
 	}
-}
 ?>
